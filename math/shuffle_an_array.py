247a8cb00ee9a789621d14a2dab771fd21d14b88
lixiangcliff <lixiang.cliff@gmail.com>
2016-11-08 07:45:36 -0800

    Added shuffle_an_array

---
+++ math/shuffle_an_array.py
@@ -0,0 +1,73 @@
+"""
+384. Shuffle an Array
+
+Shuffle a set of numbers without duplicates.
+
+Example:
+
+// Init an array with set 1, 2, and 3.
+int[] nums = {1,2,3};
+Solution solution = new Solution(nums);
+
+// Shuffle the array [1,2,3] and return its result. Any permutation of [1,2,3] must equally likely to be returned.
+solution.shuffle();
+
+// Resets the array back to its original configuration [1,2,3].
+solution.reset();
+
+// Returns the random shuffling of array [1,2,3].
+solution.shuffle();
+"""
+from random import randint
+
+
+class Solution(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        :type size: int
+        """
+        self.nums = nums
+
+    def reset(self):
+        """
+        Resets the array to its original configuration and return it.
+        :rtype: List[int]
+        """
+        return self.nums
+
+    def shuffle(self):
+        """
+        Returns a random shuffling of the array.
+        :rtype: List[int]
+        """
+        shuffled_nums = []
+        copied_nums = list(self.nums)
+        while copied_nums:
+            shuffled_nums.append(copied_nums.pop(randint(0, len(copied_nums) - 1)))
+        return shuffled_nums
+
+
+"""
+A hacky way
+https://discuss.leetcode.com/topic/54049/python-hack
+
+    def __init__(self, nums):
+        self.reset = lambda: nums
+        self.shuffle = lambda: random.sample(nums, len(nums))
+
+"""
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
+
+
+nums = [1, 2, 3]
+obj = Solution(nums)
+param_1 = obj.reset()
+param_2 = obj.shuffle()
+print param_1
+print param_2
--- test_dir/test.py
+++ test_dir/test.py
@@ -1,1 +1,24 @@
-print "this is python repo! "
+class Solution(object):
+    #nums
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        :type size: int
+        """
+        self.nums = nums
+
+
+    def reset(self):
+        """
+        Resets the array to its original configuration and return it.
+        :rtype: List[int]
+        """
+        return self.nums
+
+nums = [1, 2, 3]
+obj = Solution(nums)
+param_1 = obj.reset()
+#print obj.nums
+
+print param_1
